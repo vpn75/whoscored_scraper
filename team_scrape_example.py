2eab7b97d715426a22004ce5b35f534369478fe1
Vamsi Nath <vpnath@gmail.com>
2019-09-11 16:27:15 -0400

    -Fixed package import

--- team_scrape_example.py
+++ team_scrape_example.py
@@ -1,8 +1,7 @@
-from whoscored_scraper import *
+from whoscored_scraper import WhoScored_scraper
 
 team = 'Arsenal'
 league = 'EPL' #Supported leagues: EPL, Bundesliga, Ligue1, LaLiga, SerieA
 
 scraper = WhoScored_scraper(team, league)
 scraper.do_work()
-
